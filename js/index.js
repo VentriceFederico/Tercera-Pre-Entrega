a412eef9a590e9f5b5892d86fd03c48ce76db419
ventrice federico <ventricefederico@gmail.com>
2023-05-01 20:37:40 -0300

    Se hicieron funciones de borrado y editar.

--- js/index.js
+++ js/index.js
@@ -81,62 +81,104 @@ function crearViaje() {
         e.preventDefault();
         let id = parseInt((localStorage.getItem("id"))) || 0;
         const datos = e.target.children;
-        const viaje = new Viaje(
-            datos["nombre"].value,
-            datos["origen"].value,
-            datos["destino"].value,
-            datos["fechaIda"].value,
-            datos["fechaVuelta"].value,
-            datos["pasajeros"].value,
-            id,
-            0,
-            0,
-        )
-        viajes.push(viaje);
-        viaje.viajeEstadia(viaje.fechaIda, viaje.fechaVuelta);
-        viaje.viajePrecio();
-        id++;
-        localStorage.setItem("viajes", JSON.stringify(viajes));
-        localStorage.setItem("id",id);
-        data.reset();
-        verViaje(viaje);
-        Swal.fire({
-            title: 'viaje Ingresado!',
-            text: 'Buenas vacaciones!!!',
-            icon: 'success',
-            confirmButtonText: 'Cool'
-          })
+        if (checkOrigen(datos["origen"].value) || checkDestino(datos["destino"].value)) {
+            data.reset();
+        }
+        else {
+            const viaje = new Viaje(
+                datos["nombre"].value,
+                datos["origen"].value,
+                datos["destino"].value,
+                datos["fechaIda"].value,
+                datos["fechaVuelta"].value,
+                datos["pasajeros"].value,
+                id,
+                0,
+                0,
+            )
+            viajes.push(viaje);
+            viaje.viajeEstadia(viaje.fechaIda, viaje.fechaVuelta);
+            viaje.viajePrecio();
+            id++;
+            localStorage.setItem("viajes", JSON.stringify(viajes));
+            localStorage.setItem("id", id);
+            data.reset();
+            verViaje(viaje);
+            Swal.fire({
+                title: 'viaje Ingresado!',
+                text: 'Buenas vacaciones!!!',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+            })
+        }
     })
 }
 
 function imprimirViaje() {
-    
+
     viajes.forEach(viaje => {
         verViaje(viaje);
     });
 }
 
-function verViaje(viaje){
+function verViaje(viaje) {
     let fecha1 = new Date(viaje.fechaIda);
     let fecha2 = new Date(viaje.fechaVuelta);
 
     const contenedorViajes = document.querySelector("#viajes");
     const tarjetaViaje = document.createElement("div");
-        tarjetaViaje.className = "vuelos-tarjeta";
-        tarjetaViaje.id = "viaje_tarjeta_" + viaje.id;
-        tarjetaViaje.innerHTML = `
+    tarjetaViaje.className = "vuelos-tarjeta";
+    tarjetaViaje.id = "viaje_tarjeta_" + viaje.id;
+    tarjetaViaje.innerHTML = `
                                     <img src="./img/tarjeta_img.jpg" alt="img_tarjeta">
                                     <span class="form-title-barra"></span>
                                     <h3>Titular: ${viaje.nombre}</h3>
                                     <p>Origen: ${viaje.origen}</p>
                                     <p>Destino: ${viaje.destino}</p>
-                                    <p>Fecha Ida: ${fecha1.toLocaleDateString("es-AR", {day:"numeric", month:"short", year: "2-digit"})}</p>
-                                    <p>Fecha Vuelta: ${fecha2.toLocaleDateString("es-AR", {day:"numeric", month:"short", year: "2-digit"})}</p>
+                                    <p>Fecha Ida: ${fecha1.toLocaleDateString("es-AR", { day: "numeric", month: "short", year: "2-digit" })}</p>
+                                    <p>Fecha Vuelta: ${fecha2.toLocaleDateString("es-AR", { day: "numeric", month: "short", year: "2-digit" })}</p>
                                     <p>Estadia: ${viaje.estadia} Dias</p>
                                     <p>Cantidad de pasajeros: ${viaje.pasajeros}</p>
                                     <span>$${viaje.precio}</span>
                                 `
-        contenedorViajes.append(tarjetaViaje);
+    contenedorViajes.append(tarjetaViaje);
+}
+
+function checkOrigen(origen) {
+    if (origen == ORIGEN_1 || origen == ORIGEN_2) {
+        return false;
+    }
+    else {
+        Swal.fire({
+            title: 'Todavia no salimos de ese lugar :(',
+            icon: 'error',
+            confirmButtonText: ':('
+        })
+        return true;
+    }
+}
+
+function checkDestino(destino) {
+    console.log(destino);
+    switch (destino) {
+        case DESTINO_1:
+            return false;
+        case DESTINO_2:
+            return false;
+        case DESTINO_3:
+            return false;
+        case DESTINO_4:
+            return false;
+        case DESTINO_5:
+            return false;
+        default:
+            Swal.fire({
+                title: 'Todavia no llegamos a ese destino :(',
+                icon: 'error',
+                confirmButtonText: ':('
+            })
+            return true;
+    }
 }
 
 crearViaje();
